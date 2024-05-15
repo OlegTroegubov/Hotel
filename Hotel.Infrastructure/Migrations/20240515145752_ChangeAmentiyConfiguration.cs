40033d91168e5e01173e98f71db0e9aa0a464d2c
OlegTroegubov <mrolego6133@gmail.com>
2024-05-15 21:59:36 +0700

    Change amenity configuration

---
+++ Hotel.Infrastructure/Migrations/20240515145752_ChangeAmentiyConfiguration.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Hotel.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeAmentiyConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RoomAmenity");
+
+            migrationBuilder.CreateTable(
+                name: "RoomAmenities",
+                columns: table => new
+                {
+                    RoomId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AmenityId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomAmenities", x => new { x.AmenityId, x.RoomId });
+                    table.ForeignKey(
+                        name: "FK_RoomAmenities_Amenities_AmenityId",
+                        column: x => x.AmenityId,
+                        principalTable: "Amenities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RoomAmenities_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Amenities",
+                columns: new[] { "Id", "Title" },
+                values: new object[,]
+                {
+                    { 1, "AirConditioning" },
+                    { 2, "Patio" },
+                    { 3, "PrivateBathroom" },
+                    { 4, "FlatScreenTv" },
+                    { 5, "Soundproofing" },
+                    { 6, "CoffeeMachine" },
+                    { 7, "Minibar" },
+                    { 8, "FreeWiFi" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomAmenities_RoomId",
+                table: "RoomAmenities",
+                column: "RoomId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RoomAmenities");
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.CreateTable(
+                name: "RoomAmenity",
+                columns: table => new
+                {
+                    AmenityId = table.Column<int>(type: "integer", nullable: false),
+                    RoomId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomAmenity", x => new { x.AmenityId, x.RoomId });
+                    table.ForeignKey(
+                        name: "FK_RoomAmenity_Amenities_AmenityId",
+                        column: x => x.AmenityId,
+                        principalTable: "Amenities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RoomAmenity_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomAmenity_RoomId",
+                table: "RoomAmenity",
+                column: "RoomId");
+        }
+    }
+}
